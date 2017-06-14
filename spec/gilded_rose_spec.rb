568b3d15ea77ecef8f8573e20987654a6ce00501
Unai Motriko <unaimotriko@gmail.com>
2017-06-14 22:15:45 +0100

    Adds a test to assert increase variation dependant on sell_in

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,9 +4,21 @@ describe GildedRose do
 
   describe 'AGED BRIE' do
 
+    context 'when the sell_in is higher than 0' do
+      it 'adds one to the quality ' do
+        items = [Item.new('Aged Brie', 2, 0)]
+        expect { GildedRose.new(items).update_quality }.to change {
+          items[0].sell_in
+        }.by(-1)
+        expect { GildedRose.new(items).update_quality }.to change {
+          items[0].quality
+        }.by(1)
+      end
+    end
+
     context 'when the sell_in date is 0' do
 
-      it 'it doubles in quality next day' do
+      it 'it increases two quality next day' do
         items = [Item.new('Aged Brie', 0, 0)]
         expect { GildedRose.new(items).update_quality }.to change {
           items[0].sell_in
