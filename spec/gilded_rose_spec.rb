a30b616f6f8e6af4b0e628312d289769b0b628e4
Unai Motriko <unaimotriko@gmail.com>
2017-06-14 22:36:05 +0100

    Sets up tests for Backstage items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,34 +6,53 @@ describe GildedRose do
 
     context 'when the sell_in is higher than 0' do
       it 'adds one to the quality ' do
-        items = [Item.new('Aged Brie', 2, 0)]
-        expect { GildedRose.new(items).update_quality }.to change {
-          items[0].sell_in
+        item = [Item.new('Aged Brie', 2, 0)]
+        expect { GildedRose.new(item).update_quality }.to change {
+          item[0].sell_in
         }.by(-1)
-        expect { GildedRose.new(items).update_quality }.to change {
-          items[0].quality
+        expect { GildedRose.new(item).update_quality }.to change {
+          item[0].quality
         }.by(1)
       end
     end
 
     context 'when the sell_in date is 0' do
-
       it 'it increases two quality next day' do
-        items = [Item.new('Aged Brie', 0, 0)]
-        expect { GildedRose.new(items).update_quality }.to change {
-          items[0].sell_in
+        item = [Item.new('Aged Brie', 0, 0)]
+        expect { GildedRose.new(item).update_quality }.to change {
+          item[0].sell_in
         }.by(-1)
-        expect { GildedRose.new(items).update_quality }.to change {
-          items[0].quality
+        expect { GildedRose.new(item).update_quality }.to change {
+          item[0].quality
         }.by(2)
+        end
+      end
+
+      context 'when the value of quality is already 50' do
+        it 'it does not increase in value further' do
+          item = [Item.new('Aged Brie', 0, 50)]
+          expect { GildedRose.new(item).update_quality }.to change {
+            item[0].sell_in
+          }.by(-1)
+          expect { GildedRose.new(item).update_quality }.to change {
+            item[0].quality
+          }.by(0)
         end
       end
     end
 
-  #   it 'does not exceed 50 when updating non Sulfuras item' do
-  #     items = [Item.new('Aged Brie', 0, 50)]
-  #     expect { GildedRose.new(items).update_quality }.to_not change {
-  #       items[0].quality
-  #     }.by(1)
-  # end
+    describe 'BACKSTAGE PASSES' do
+
+      context 'when the sell_in is higher than 11' do
+        it 'the quality increases by one' do
+          item = [Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 2)]
+          expect { GildedRose.new(item).update_quality }.to change {
+            item[0].sell_in
+          }.by(-1)
+          expect { GildedRose.new(item)}
+        end
+      end
+    end
+
+
 end
