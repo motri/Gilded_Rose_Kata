bc28b3e59930290b3e4008a4d1ed024b033db18d
Unai Motriko <unaimotriko@gmail.com>
2017-06-14 20:00:45 +0100

    Passes sencond test asserting sell_in change for Aged Brie

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,13 +1,18 @@
 require 'gilded_rose'
 
 describe GildedRose do
+  describe '#update_quality' do
+    it 'does not change the name' do
+      items = [Item.new('foo', 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq 'foo'
+    end
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+    it 'changes sell_in for the recognised items' do
+      items = [Item.new('Aged Brie', 0, 0)]
+      expect { GildedRose.new(items).update_quality }.to change {
+        items[0].sell_in
+      }.by(-1)
     end
   end
-
 end
