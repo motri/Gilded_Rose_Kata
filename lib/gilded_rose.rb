8374119a34888c482f9791415c69705b9db40ba7
Unai Motriko <unaimotriko@gmail.com>
2017-06-15 14:36:58 +0100

    Separates concerns

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -21,12 +21,19 @@ class GildedRose
   end
 
   def update_regular(item)
+    item.sell_in -= 1
+    return if item.quality == 0
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in <= 0
+
   end
 
   def update_quality
     @items.each do |item|
+      next if item.name == 'Sulfuras, Hand of Ragnaros'
       update_brie(item) if item.name == 'Aged Brie'
       update_pass(item) if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      update_regular(item) if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
     end
   end
 end
