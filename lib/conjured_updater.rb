1505368dcef5fafedaf850a6f22554b104f427c0
Unai Motriko <unaimotriko@gmail.com>
2017-06-17 16:24:13 +0100

    Updates Conjured class to invoke RegularUdater

--- lib/conjured_updater.rb
+++ lib/conjured_updater.rb
@@ -5,21 +5,6 @@ class Conjured
   end
 
   def update
-    return if @item.quality.zero?
-    sell_in_valid ? update_fresh : udpdate_expired
-  end
-
-  private
-
-  def sell_in_valid
-    @item.sell_in >= 0
-  end
-
-  def update_fresh
-    2.times { @item.quality -= 1 unless @item.quality.zero? }
-  end
-
-  def udpdate_expired
-    4.times { @item.quality -= 1 unless @item.quality.zero? }
+    2.times { RegularUpdater.new(@item).update }
   end
 end
